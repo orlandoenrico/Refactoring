6aafd371b6204b376acf05eb46a901d165555dd7
Orlando Enrico <36771949+orlandoenrico@users.noreply.github.com>
2019-11-10 23:05:09 -0300

    Commit #10

--- src/Rental.java
+++ src/Rental.java
@@ -3,42 +3,23 @@ class Rental {
     private int _daysRented;
 
     public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
+        _movie = movie;
+        _daysRented = daysRented;
     }
+
     public int getDaysRented() {
-      return _daysRented;
+        return _daysRented;
     }
+
     public Movie getMovie() {
-      return _movie;
+        return _movie;
     }
+    
     public double getCharge() {
-        double result = 0;
-
-        //determine amounts for rental line
-        switch (getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-            result += 2;
-            if (getDaysRented() > 2)
-                result += (getDaysRented() - 2) * 1.5;
-            break;
-        case Movie.NEW_RELEASE:
-            result += getDaysRented() * 3;
-            break;
-        case Movie.CHILDRENS:
-            result += 1.5;
-            if (getDaysRented() > 3)
-                result += (getDaysRented() - 3) * 1.5;
-            break;
-        }
-
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     public int getFrequentRenterPoints() {
-       if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-          return 2;
-       else
-          return 1;
-   }
+        return _movie.getFrequentRenterPoints(_daysRented);
+    }
 }
